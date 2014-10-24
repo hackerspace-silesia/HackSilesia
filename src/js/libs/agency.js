f9ad2ad1127137e47216d1772e9aabdd1f5050a9
Dariusz Niemczyk <palid@o2.pl>
2014-10-24 12:22:57 +0200

    Smooth-scroll anchors changed.

--- src/js/libs/agency.js
+++ src/js/libs/agency.js
@@ -11,7 +11,7 @@ $('document').ready(function(){
         $('a.page-scroll').bind('click', function (event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
+                scrollTop: $($anchor.attr('href')).offset().top - 50
             }, 1500, 'easeInOutExpo');
             event.preventDefault();
         });
