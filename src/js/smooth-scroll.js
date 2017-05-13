8b55103196379e5cf7a794b646902441c9afa67e
Dariusz Niemczyk <palid@o2.pl>
2017-05-13 16:13:44 +0200

    Smoothscroll fixes

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,21 +1,28 @@
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+(function(doc, win, $) {
+  function getOffset(offset) {
+    var newOffset = offset - 50;
+    return newOffset > 0 ? newOffset : offset;
+  }
+  $(doc).ready(function() {
+    $('.scrollspy a[href^="#"]').bind('click.smoothscroll', function(e) {
+      e.preventDefault();
+      var target = this.hash,
+        $target = $(target),
+        offset = $target.offset().top || 0;
+
+      $('html, body').stop().animate({
+        'scrollTop': getOffset(offset)
+      }, 1500, 'swing', function() {
+        win.location.hash = target;
+      });
+    });
+    $(win).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
+      } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+      }
+    });
+  });
 
-jQuery(document).ready(function($) {
-    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
-        e.preventDefault();
-        var target = this.hash,
-        $target = $(target);
-
-        $('html, body').stop().animate( {
-            'scrollTop': $target.offset().top
-        }, 1500, 'swing', function () {
-            window.location.hash = target;
-        } );
-    } );
-} );
+})(document, window, jQuery);
